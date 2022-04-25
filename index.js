c56e434b847ea8d62c9b64837dfad0104f659fc8
Lada Šilhavá <lada.silhava@email.cz>
2022-04-25 08:38:39 +0200

    responzivita úprava

--- index.js
+++ index.js
@@ -3,19 +3,18 @@ let player = 'circle';
 const cell = document.querySelectorAll('button');
 
 for (let i = 0; i < cell.length; i +=1 ) {
-    cell[i].addEventListener('click',  (ev) => {
+    cell[i].addEventListener('click', (ev) => {
             if (player === 'circle') {
                     ev.target.classList.add('board__field--circle');
-                    const playerIcon = document.querySelector('.icon');
+                    const playerIcon = document.querySelector('.icon--player');
                     playerIcon.src = 'cross.svg';
                     player = 'cross';
             } else {
                     ev.target.classList.add('board__field--cross');
-                    const playerIcon = document.querySelector('.icon');
+                    const playerIcon = document.querySelector('.icon--player');
                     playerIcon.src = 'circle.svg';
                     player = 'circle';
             }
             cell[i].disabled = true
         });
 };
-
